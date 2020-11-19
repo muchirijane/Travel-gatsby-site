f3ab5a4176e5e2cc97e3c4ea368babda3d907110
Jane Tracy Muthoni <muchirijane@outlook.com>
2020-11-19 08:18:50 +0300

    Updates

---
+++ 1a48c3c1-3dd19b148581ac143977.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{qtta:function(t,n,a){"use strict";a.d(n,"a",(function(){return i}));var c=a("Lnxd");function i(t){return Object(c.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M4 6H20V8H4zM4 11H20V13H4zM4 16H20V18H4z"}}]})(t)}}}]);
+//# sourceMappingURL=1a48c3c1-3dd19b148581ac143977.js.map
